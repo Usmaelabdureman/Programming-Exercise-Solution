92a5201ac931da30c9f44eb15640d628e9c73e99
Usmaelabdureman <uabdureman@gmail.com>
2022-04-15 18:20:49 +0300

    Solution to Question #17.1

---
+++ QuickSort.py
@@ -0,0 +1,42 @@
+
+def quickSort(ListToSort):
+    QuickSortHlper(ListToSort,0,len(ListToSort)-1)
+    
+#Quicksort helper function do recursively
+def QuickSortHlper(list,low,high):
+    if len(list)==1:
+        return list
+
+    if low< high:
+        pivot= get_median([list[low], list[high], list[(high+low)//2]])
+        pivotIndex = partition(list,low,high,pivot)
+        QuickSortHlper(list,low,pivotIndex-1)
+        QuickSortHlper(list,pivotIndex,high)
+
+#let's find Median of the first , middle and last number in array        
+def get_median(list):
+    for i in range(len(list)):
+        for j in range(i+1):
+            if list[i] > list[j]:
+                list[i],list[j] = list[j],list[i]
+    return list[1]
+#Parition the array into two parts recursive
+def partition(list, low, high, pivot):
+    while low <= high:
+        while list[low] < pivot:
+            low += 1
+        while list[high] >pivot:
+            high -= 1 
+        if low <= high:
+            list[low], list[high] = list[high], list[low]
+            low += 1
+            high -=1
+    return low
+
+def test():
+    ll=[5,3,6,-2,-5,0,8,45,60,96,28,11,6]
+    print("list before sort:",ll)
+    quickSort(ll)
+    print("List after sorted:",ll)
+if __name__=="__main__":
+    test()
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Programming-Exercise-Solution
+# Data Structure using python Programming 
+
+
+    Name : Usmael Abdurhaman
+    ID : UGR/6575/12
+
+
+        #   Programming Exercise 
+(17.1
+(Improving quick sort) The quick sort algorithm presented in the book
+selects the first element in the list as the pivot. Revise it by
+selecting the median among the first, middle, and last elements in
+the list.)
